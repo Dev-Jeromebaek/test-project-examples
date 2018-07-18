ad5ad28e4c3af4115e4351e35bcbc0da2357ef59
백승엽 <jerome@tmon.co.kr>
2018-07-18 10:06:29 +0900

    20180718

--- contact-app/src/components/PhoneForm.js
+++ contact-app/src/components/PhoneForm.js
@@ -23,41 +23,40 @@ class PhoneForm extends Component {
         /*this.props.onCreate({
             name: this.state.name,
             phone: this.state.phone,
-        })
-        */
-       this.props.onCreate(this.state);
-       this.setState({
-           name: '',
-           phone: '',
-       });
-       this.input.focus();
-       //16버전이상
-       /*this.input.current.focus();*/
+        })        */
+        this.props.onCreate(this.state);
+        this.setState({
+            name: '',
+            phone: '',
+        });
+        this.input.focus();
+        //16버전이상
+        /*this.input.current.focus();*/
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input 
-                    name="name" 
-                    placeholder="이름" 
-                    onChange={this.handleChange} 
+                <input
+                    name="name"
+                    placeholder="이름"
+                    onChange={this.handleChange}
                     value={this.state.name}
                     ref={ref => this.input = ref}
-                    //16버전이상
-                    /*ref={this.input}*/
+                //16버전이상
+                /*ref={this.input}*/
                 />
-                <input 
-                    name="phone" 
-                    placeholder="전화번호" 
-                    onChange={this.handleChange} 
+                <input
+                    name="phone"
+                    placeholder="전화번호"
+                    onChange={this.handleChange}
                     value={this.state.phone}
                 />
                 <button type="submit">등록</button>
                 <div>
-                {this.state.name} {this.state.phone}
+                    {this.state.name} {this.state.phone}
                 </div>
-                
+
             </form>
         );
     }
