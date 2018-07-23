ebd82f2e7c51f22f6086dccde2ed50146c58a454
Dev-JeromeBaek <superior051@icloud.com>
2018-07-24 00:56:35 +0900

    20180724 meterial design exam

---
+++ react-study-jerome/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MiniDrawer from './components/header/MiniDrawer';
+
+class App extends Component {
+  render() {
+    return <MiniDrawer />;
+  }
+}
+
+export default App;
