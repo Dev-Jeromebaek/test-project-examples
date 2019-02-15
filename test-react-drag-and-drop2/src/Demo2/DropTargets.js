e7b50360c3bb98f26474d15a1b44f4e826ca67b8
JeromeBaek <superior051@icloud.com>
2019-02-16 03:29:12 +0900

    docs: Remove test dnd2 project

--- test-react-drag-and-drop2/src/Demo2/DropTargets.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class DropTargets extends Component {
-  render() {
-    return <div>DropTargets</div>;
-  }
-}
-
-export default DropTargets;
--- test-react-drag-and-drop2/src/Demo2/SourceObject.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class SourceObject extends Component {
-  render() {
-    return <div>SourceObject</div>;
-  }
-}
-
-export default SourceObject;
