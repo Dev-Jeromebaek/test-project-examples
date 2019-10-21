f93802b4c210d01dc42e800b89f9f8a963fc0c5a
JeromeBaek <superior051@icloud.com>
2019-10-21 17:09:11 +0900

    feat: Add new TDD project basic-js-test-jest

---
+++ tdd-projects/basic-js-test-jest/sum.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function sumOf(numbers) {
+  // let result = 0;
+  // numbers.forEach(n => {
+  //   result += n;
+  // });
+  // return result;
+  // 리팩토링
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
+
+// module.exports = sum;
+exports.sum = sum;
+exports.sumOf = sumOf;
