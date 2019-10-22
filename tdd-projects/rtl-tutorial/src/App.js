11ba18feb046b579253a66b9a4abfc8fd7818cf1
JeromeBaek <superior051@icloud.com>
2019-10-22 17:53:52 +0900

    feat: Add testing of REST API

--- tdd-projects/rtl-tutorial/src/App.js
+++ tdd-projects/rtl-tutorial/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Profile from './Profile';
 import Counter from './Counter';
+//! async 테스트
+import DelayedToggle from './DelayedToggle';
+//! REST API 테스트 (with jsonplaceholder)
+import UserProfile from './UserProfile';
 
 function App() {
   return (
     <>
       <Profile username="jeromeBaek" name="백승엽" />
       <Counter />
+      <DelayedToggle />
+      <UserProfile id={1} />
     </>
   );
 }
