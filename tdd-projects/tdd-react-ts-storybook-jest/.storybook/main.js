35d93504c4f49488046dab8ee52317f32c62406d
seungyeob <superior051@icloud.com>
2020-04-23 12:08:31 +0900

    feat: Add Button, ButtonGroup, SVGIcon style guide

--- tdd-projects/tdd-react-ts-storybook-jest/.storybook/main.js
+++ tdd-projects/tdd-react-ts-storybook-jest/.storybook/main.js
@@ -1,5 +1,3 @@
-const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
-
 module.exports = {
   stories: ['../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
   addons: [
@@ -17,6 +15,19 @@ module.exports = {
           loader: require.resolve('babel-loader'),
           options: {
             presets: [['react-app', { flow: false, typescript: true }]],
+            plugins: [
+              [
+                require.resolve('babel-plugin-named-asset-import'),
+                {
+                  loaderMap: {
+                    svg: {
+                      ReactComponent:
+                        '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                    },
+                  },
+                },
+              ],
+            ],
           },
         },
         {
